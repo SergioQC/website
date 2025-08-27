b6eee5d635aec020128e7a38db1ffc377cf58fc6
Sergio Quintana <s.quintana.c@gmail.com>
2025-08-27 23:23:34 +0200

    Redirection

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, resolve, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,5 +21,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      input: { main: resolve(__dirname, 'index.html'), 404: resolve(__dirname, 'public/404.html') },
+    },
+  },
   base: '/',
-})
+});
