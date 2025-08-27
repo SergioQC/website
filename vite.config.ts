569ab84385e1d05b67f8019f8ae67a925fc83a81
Sergio Quintana <s.quintana.c@gmail.com>
2025-08-28 00:33:32 +0200

    more 404

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: { main: resolve(__dirname, 'index.html'), 404: resolve(__dirname, 'public/404.html') },
+      input: { main: resolve(__dirname, 'index.html'), 404: resolve(__dirname, '404.html') },
     },
   },
   base: '/',
